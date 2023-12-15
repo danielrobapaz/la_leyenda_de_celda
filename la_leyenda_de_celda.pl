c61686fdb8c16d818467296cede467f89f503bc6
Santiago Finamore <18-10125@usb.ve>
2023-12-15 11:59:29 -0400

    Empezado trabajo en cliente interactivo

--- la_leyenda_de_celda.pl
+++ la_leyenda_de_celda.pl
@@ -65,3 +65,35 @@ cruzar_por_casos(bifurcacion(Sub_Mapa_1, Sub_Mapa_2), Palancas, trampa) :-
 % SECCION SIEMPRE SEGURO
 
 siempre_seguro(Mapa) :- \+(cruzar(Mapa, _, trampa)).
+
+% LECTURA DE ARCHIVOS
+leer(X) :- read(MapPath), see(MapPath), read(X), seen.
+
+% INTERACTIVIDAD CON EL USUARIO
+
+inicio :-
+    write('Hey, listen! Hiperlink, rápido, ¿Dónde está el mapa?: '),
+    leer(Mapa),
+    menu_principal(Mapa).
+
+menu_principal(Mapa) :-
+    write('¿Qué quieres hacer con el mapa? 1. Cruzar 2. Verificar si es siempre seguro: '),
+    read(Opcion),
+    (Opcion == 1 -> menu_cruzar(Mapa); siempre_seguro(Mapa)).
+
+menu_cruzar(Mapa) :-
+    write('¿Quieres dar un conjunto de palancas y ver el resultado, o ver las palancas que llevan a un estado? 1. Dar conjunto de palancas 2. Dar estado final: '),
+    read(Opcion),
+    (Opcion == 1 -> dar_palancas(Mapa); ver_palancas(Mapa)).
+
+dar_palancas(Mapa) :-
+    write('Por favor, introduce el conjunto de palancas: '),
+    read(Palancas),
+    cruzar(Mapa, Palancas, Resultado),
+    (Resultado == true -> write('Es seguro cruzar con estas palancas :)'); write('No es seguro cruzar, ¡Cuidado, Hyperlink! D:')).
+
+ver_palancas(Mapa) :-
+    write('¿Quieres ver las palancas que llevan a un estado seguro o a una trampa? 1. Seguro 2. Trampa: '),
+    read(Opcion),
+    (Opcion == 1 -> cruzar(Mapa, P, seguro); cruzar(Mapa, P, trampa)),
+    write(P).
